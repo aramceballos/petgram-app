dbd7a5e963adb913a1cf1ce37287976076c14e41
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-07-21 14:26:27 -0700

    :sparkles: Add Post screen to User stack

--- src/stacks/User.tsx
+++ src/stacks/User.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import User from '../screens/User';
+import Post from '../screens/Post';
 import Header from '../components/Header';
 
 const Stack = createStackNavigator();
@@ -14,6 +15,14 @@ const UserStack = () => {
         component={User}
         options={{
           headerTitle: () => <Header />,
+        }}
+      />
+      <Stack.Screen
+        name="Post"
+        component={Post}
+        options={{
+          headerTitle: () => <Header />,
+          headerStyle: { shadowColor: 'transparent' },
         }}
       />
     </Stack.Navigator>
