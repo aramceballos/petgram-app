9c6b9b346c2ac734b7a41d78ddd9436d257671f5
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-07-21 18:55:10 -0700

    :sparkles: Add Favorites screen

--- src/screens/Favorites.tsx
+++ src/screens/Favorites.tsx
@@ -1,12 +1,77 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Dimensions } from 'react-native';
+import { connect } from 'react-redux';
+import styled from 'styled-components/native';
+import axios from 'axios';
 
-const Favorites = () => {
+import { setToken as setTokenAction } from '../actions';
+
+const PostsContainer = styled.View`
+  width: ${Dimensions.get('window').width}px;
+  flex-direction: row;
+  flex-wrap: wrap;
+`;
+
+const PostImageWrapper = styled.Pressable`
+  height: ${Dimensions.get('window').width / 3}px;
+  width: ${Dimensions.get('window').width / 3}px;
+`;
+
+const PostImage = styled.Image`
+  height: ${Dimensions.get('window').width / 3}px;
+  width: ${Dimensions.get('window').width / 3}px;
+`;
+
+const Favorites = ({ navigation, token, setToken }) => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    getPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getPosts = async () => {
+    try {
+      const res = await axios('https://api.petgram.club/api/p/f', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setPosts(res.data.data);
+    } catch (error) {
+      if (
+        error.response?.data?.message === 'Missing or malformed JWT' ||
+        error.response?.data?.message === 'Invalid or expired JWT'
+      ) {
+        setToken('');
+      }
+      console.error(error.response?.data?.message);
+    }
+  };
+
+  const handlePress = (postId: number) => {
+    navigation.navigate('Post', { postId });
+  };
+
   return (
-    <View>
-      <Text>This is the Favorites screen</Text>
-    </View>
+    <PostsContainer scrollView>
+      {posts &&
+        posts.length > 0 &&
+        posts.map((post) => (
+          <PostImageWrapper key={post.id} onPress={() => handlePress(post.id)}>
+            <PostImage source={{ uri: post.image_url }} />
+          </PostImageWrapper>
+        ))}
+    </PostsContainer>
   );
 };
 
-export default Favorites;
+const mapStateToProps = (state) => ({
+  token: state.token,
+});
+
+const mapDispatchToProps = {
+  setToken: setTokenAction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
--- src/stacks/Favorites.tsx
+++ src/stacks/Favorites.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Favorites from '../screens/Favorites';
+import Post from '../screens/Post';
 import Header from '../components/Header';
 
 const Stack = createStackNavigator();
@@ -14,6 +15,14 @@ const FavoritesStack = () => {
         component={Favorites}
         options={{
           headerTitle: () => <Header />,
+        }}
+      />
+      <Stack.Screen
+        name="Post"
+        component={Post}
+        options={{
+          headerTitle: () => <Header />,
+          headerStyle: { shadowColor: 'transparent' },
         }}
       />
     </Stack.Navigator>
