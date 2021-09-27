726bad7e531d1ab62554cef468ddebbc89903da8
Aram Ceballos <aramgonzalez12@hotmail.com>
2021-09-27 16:57:21 -0700

    :bug: Fix wrong user posts api url

--- src/screens/User.tsx
+++ src/screens/User.tsx
@@ -85,7 +85,7 @@ const User = ({ navigation, token, setToken, userInfo }) => {
   const getPosts = async (userId: string) => {
     try {
       const res = await axios(
-        `https://petgram-api-aram.herokuapp.com/api/post?user_id=${userId}`,
+        `https://petgram-api-aram.herokuapp.com/api/posts?user_id=${userId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
